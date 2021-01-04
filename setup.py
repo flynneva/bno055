8e7e0b6448b927f0ce00cfe505815be2667ef37f
whatis777 <novotny@software-novotny.de>
2021-01-04 20:39:10 +0100

    fixed ament_python warning by adding data_files

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ setup(
     py_modules=[
         package_name,
     ],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     author='flynneva',
